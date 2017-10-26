dc96ef9cd2aea06b51a7a5173afab77f335e1f5c
Swathi <swathi.nemana@gmail.com>
2017-10-26 20:46:26 +0530

    committed

---
+++ DiagonalLotusHeights/DiagonalLotusHeights/scripts/Custom.js
@@ -0,0 +1,13 @@
+﻿
+        function initMap() {
+            var uluru = { lat: 17.7348583, lng: 83.31618189999995 };
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 4,
+                center: uluru
+            });
+            var marker = new google.maps.Marker({
+                position: uluru,
+                map: map
+            });
+        }
+ 
